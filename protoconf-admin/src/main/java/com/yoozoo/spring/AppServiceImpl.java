1de112d40ee2a78e6def45bc21420d78b4d954e5
wuhongbo <wuhongbo@yoozoo.com>
2018-07-23 12:25:17 +0800

    add protoconf java sdk and get/set key value in etcd server

--- protoconf-admin/src/main/java/com/yoozoo/spring/AppServiceImpl.java
+++ protoconf-admin/src/main/java/com/yoozoo/spring/AppServiceImpl.java
@@ -1,71 +1,128 @@
 package com.yoozoo.spring;
 
+import com.yoozoo.protoconf.ConfigurationReader;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @CrossOrigin
 public class AppServiceImpl extends AppServiceBase {
+    @Autowired
+    ConfigurationReader configurationReader;
+
     @Override
     GroupListResponse GetGroups(GroupListRequest in) {
-        return new GroupListResponse(Application.groups);
+        Map<String, String> keyValues = configurationReader.getValuesWithPrefix("/group");
+        List<Group> groups = new ArrayList<>();
+
+        if(keyValues == null || keyValues.isEmpty()) {
+//            no key-value found
+            return null;
+        }
+
+        for(Map.Entry<String, String> entry: keyValues.entrySet()) {
+            if(entry.getKey().split("/").length == 2) {
+//                get group only
+                groups.add(new Group(entry.getKey(), entry.getValue()));
+            }
+        }
+
+        if(groups.isEmpty()) {
+//            no group found
+            return null;
+        }
+        return new GroupListResponse(groups);
     }
 
     @Override
     AppListResponse GetApps(AppListRequest in) {
-        int startIndex = Integer.parseInt(in.getGroup_id()) - 1;
-        return new AppListResponse(Application.apps.subList(startIndex * 3, (startIndex + 1) * 3 ), in.getGroup_id(), Application.groups.get(startIndex).getGroup_name());
+        Map<String, String> keyValues = configurationReader.getValuesWithPrefix(in.getGroup_id() + "/app");
+        if(keyValues == null || keyValues.isEmpty()) {
+//            no app found
+            return null;
+        }
+        List<App> apps = new ArrayList<>();
+
+        for(Map.Entry<String, String> entry: keyValues.entrySet()) {
+            if(entry.getKey().split("/").length == 3) {
+//                get app only
+                apps.add(new App(entry.getKey(), entry.getValue()));
+            }
+        }
+
+        String groupName = configurationReader.getValue(in.getGroup_id());
+        return new AppListResponse(apps, in.getGroup_id(), groupName);
     }
 
     @Override
     KeyListResponse GetKeyList(KeyListRequest in) {
-        int appId = Integer.parseInt(in.getApp_id()) - 1;
-        List<KeyValue> keyValues = Application.keys.subList(appId*2, appId*2 + 2);
+        Map<String, String> keyValues = configurationReader.getValuesWithPrefix(in.getApp_id() + "/");
         List<String> keys = new ArrayList<>();
-        for (KeyValue keyValue : keyValues) {
-            keys.add(keyValue.getKey());
+
+        if(keyValues == null || keyValues.isEmpty()) {
+//            no key found
+            return null;
         }
-        return new KeyListResponse(keys, in.getApp_id(), Application.apps.get(appId).getApp_name());
+
+        for(Map.Entry<String, String> entry: keyValues.entrySet()) {
+            keys.add(entry.getKey());
+        }
+
+        String appName = configurationReader.getValue(in.getApp_id());
+        return new KeyListResponse(keys, in.getApp_id(), appName);
     }
 
     @Override
     KeyValueListResponse GetKeyValueList(KeyValueListRequest in) {
         List<String> keys = in.getKeys();
         List<KeyValue> keyValues = new ArrayList<>();
-        int keyId = 0;
-        int i = 0;
-        for (KeyValue keyValue : Application.keys) {
-            i++;
-            if (keys.contains(keyValue.getKey())) {
-                keyValues.add(keyValue);
-                keyId = i;
-            }
+
+        for(String key: keys) {
+            keyValues.add(new KeyValue(key, configurationReader.getValue(key)));
         }
-        int appId = (keyId - 1) /2 + 1;
-        String appName = Application.apps.get(appId - 1).getApp_name();
-        int groupId = (appId - 1) / 3 + 1;
-        String groupName = Application.groups.get(groupId - 1).getGroup_name();
-        return new KeyValueListResponse(keyValues, String.valueOf(appId), appName, String.valueOf(groupId), groupName);
+
+        if(keyValues.isEmpty()) {
+//            no key found
+            return null;
+        }
+
+        String[] keySplit = keys.get(0).split("/");
+        if(keySplit.length < 3) {
+//            wrong key format: correct format is /grp/app/*
+            return null;
+        }
+
+        String groupId = "/" + keySplit[1];
+        String groupName = configurationReader.getValue(groupId);
+
+        String appId = "/" + keySplit[1] + "/" + keySplit[2];
+        String appName = configurationReader.getValue(appId);
+
+        return new KeyValueListResponse(keyValues, appId, appName, groupId, groupName);
     }
 
     @Override
     KeyValueResponse UpdateKeyValue(KeyValueRequest in) {
-        int keyId = 0;
-        for (KeyValue keyValue : Application.keys) {
-            keyId++;
-            if (keyValue.getKey().equalsIgnoreCase(in.getKey_value().getKey())) {
-                Application.keys.set(keyId - 1, in.getKey_value());
-                int appId = (keyId - 1) /2 + 1;
-                String appName = Application.apps.get(appId - 1).getApp_name();
-                int groupId = (appId - 1) / 3 + 1;
-                String groupName = Application.groups.get(groupId - 1).getGroup_name();
-                return new KeyValueResponse(in.getKey_value(), String.valueOf(appId), appName, String.valueOf(groupId), groupName);
-            }
+//        update key-value
+        configurationReader.setValue(in.getKey_value().getKey(), in.getKey_value().getValue());
+
+        String[] keySplit = in.getKey_value().getKey().split("/");
+        if(keySplit.length < 3) {
+//            wrong key format: correct format is /grp/app/*
+            return null;
         }
 
-        return null;
+        String groupId = "/" + keySplit[1];
+        String groupName = configurationReader.getValue(groupId);
+
+        String appId = "/" + keySplit[1] + "/" + keySplit[2];
+        String appName = configurationReader.getValue(appId);
+
+        return new KeyValueResponse(in.getKey_value(), appId, appName, groupId, groupName);
     }
 }
