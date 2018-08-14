20b74b1272dcbaef40a88d2cba8eb35603de8df0
wuhongbo <wuhongbo@yoozoo.com>
2018-08-14 17:49:12 +0800

    get lang from flag

--- main.go
+++ main.go
@@ -3,11 +3,11 @@ package main
 //go:generate esc -o generator/data/tpl/tpl.go -pkg=tpl generator/template
 
 import (
+	"flag"
 	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
-	"strings"
 
 	"github.com/golang/protobuf/proto"
 	plugin "github.com/golang/protobuf/protoc-gen-go/plugin"
@@ -18,53 +18,55 @@ import (
 func main() {
 	args := os.Args
 	var input []byte
-	if len(args[1:]) > 0 {
+	// if command has parameter then call protoc, if no parameter meaning calling from protoc
+	if len(args) > 2 {
 		var err error
-		input, err = ioutil.ReadFile(args[2])
+		protoFile := args[2]
+		input, err = ioutil.ReadFile(protoFile)
 		if err != nil {
-			log.Fatalf("reading file %s error: %s\n", args[2], err.Error())
+			log.Fatalf("reading file %s error: %s\n", protoFile, err.Error())
 		}
-	} else {
-		var err error
-		input, err = ioutil.ReadAll(os.Stdin)
-		if err != nil {
-			log.Fatalf("reading stdin error: %s\n", err.Error())
-		}
-	}
-
-	// if run with protoapi then run protoc with plugin
-	if args[0] == "protoapi" {
+		// protoapi binary
 		executable, _ := os.Executable()
 
-		args[0] = "--plugin=protoc-gen-custom=" + executable
-		args[1] = strings.Replace(args[1], "--", "--custom_out=", 1)
+		langFlag := flag.String("lang", "echo:.", "output language and output directory")
+
+		flags := []string{}
+		flags = append(flags, "--plugin=protoc-gen-custom="+executable)
+		flags = append(flags, "--custom_out=lang="+*langFlag)
+		flags = append(flags, protoFile)
 
 		// Run protoc command
-		cmd := exec.Command("protoc", args...)
+		cmd := exec.Command("protoc", flags...)
 		cmd.Stderr = os.Stderr
 
 		if err := cmd.Run(); err != nil {
 			log.Fatal(err)
 		}
 
-		return
-	}
+	} else {
+		var err error
+		input, err = ioutil.ReadAll(os.Stdin)
+		if err != nil {
+			log.Fatalf("reading stdin error: %s\n", err.Error())
+		}
 
-	request := new(plugin.CodeGeneratorRequest)
+		request := new(plugin.CodeGeneratorRequest)
 
-	proto.Unmarshal(input, request)
-	if len(request.FileToGenerate) != 1 {
-		log.Fatalf("input files are： %v\nwe only support one proto file\n", request.FileToGenerate)
-	}
+		proto.Unmarshal(input, request)
+		if len(request.FileToGenerate) != 1 {
+			log.Fatalf("input files are： %v\nwe only support one proto file\n", request.FileToGenerate)
+		}
 
-	response, err := generator.Generate(request)
-	if err != nil {
-		log.Fatalf("generate response failed: %s\n", err.Error())
-	}
+		response, err := generator.Generate(request)
+		if err != nil {
+			log.Fatalf("generate response failed: %s\n", err.Error())
+		}
 
-	output, err := proto.Marshal(response)
-	if err != nil {
-		log.Fatalf("create response failed: %s\n", err.Error())
+		output, err := proto.Marshal(response)
+		if err != nil {
+			log.Fatalf("create response failed: %s\n", err.Error())
+		}
+		os.Stdout.Write(output)
 	}
-	os.Stdout.Write(output)
 }
