3808c245628c51e715d65128e32da1419cc217b7
问天 <wengw@yoozoo.com>
2018-10-12 17:13:57 +0800

    temp save

---
+++ b.bat
@@ -0,0 +1,4 @@
+.\protoapi.exe gen test\result\go\ test\proto\test.proto --lang=go
+REM .\protoapi.exe gen test\result\ts\ test\proto\test.proto --lang=ts
+REM .\protoapi.exe gen test\result\ts\fetch test\proto\test.proto --lang=ts-fetch
+REM .\protoapi.exe gen test\result\ts\axios test\proto\test.proto --lang=ts-axios
--- generator/output/go.go
+++ generator/output/go.go
@@ -1,8 +1,11 @@
 package output
 
 import (
+	"bytes"
+
 	plugin "github.com/golang/protobuf/protoc-gen-go/plugin"
 	"version.uuzu.com/Merlion/protoapi/generator/data"
+	"version.uuzu.com/Merlion/protoapi/util"
 )
 
 // Re-use everything in echoGen, only use different template
@@ -10,12 +13,48 @@ type goGen struct {
 	echoGen
 }
 
+type goService struct {
+	*echoService
+}
+
+func (s *goService) Foo(method *echoMethod) string {
+	return method.ErrorType()
+}
+
+func (s *goService) Foo1(method *echoMethod) string {
+	return method.ErrorType()
+}
+
+func (g *goGen) genGoServie(service *data.ServiceData) string {
+	buf := bytes.NewBufferString("")
+
+	obj := newEchoService(service, g.PackageName)
+	err := g.serviceTpl.Execute(buf, &goService{obj})
+	if err != nil {
+		util.Die(err)
+	}
+
+	return formatBuffer(buf)
+}
+
 func (g *goGen) Init(request *plugin.CodeGeneratorRequest) {
 	g.echoGen.Init(request)
 
 	g.structTpl = g.getTpl("/generator/template/go/struct.gogo")
-	g.serviceTpl = g.getTpl("/generator/template/go/service.gogo")
 	g.enumTpl = g.getTpl("/generator/template/go/enum.gogo")
+}
+
+func (g *goGen) Gen(applicationName string, packageName string, service *data.ServiceData, messages []*data.MessageData, enums []*data.EnumData, options data.OptionMap) (result map[string]string, err error) {
+	result, err = g.echoGen.Gen(applicationName, packageName, service, messages, enums, options)
+
+	// Temporary hack from go server gen here
+	// Should rewrite goGen completely later
+	g.serviceTpl = g.getTpl("/generator/template/go/service.gogo")
+
+	filename := genEchoFileName(g.PackageName, service)
+	content := g.genGoServie(service)
+	result[filename] = content
+	return
 }
 
 func init() {
