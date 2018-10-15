c624c9ce4a6d1637038f6f1185a7949e5715e7d6
问天 <wengw@yoozoo.com>
2018-10-15 14:51:43 +0800

    Add echo test code

---
+++ test/test_echo.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"github.com/labstack/echo"
+	"version.uuzu.com/Merlion/protoapi/test/result/go/echosvr"
+)
+
+type echoService struct{}
+
+// Echo just return req
+func (s *echoService) Echo(c echo.Context, req *echosvr.Msg) (resp *echosvr.Msg, err error) {
+	resp = req
+
+	return
+}
+
+func main() {
+	e := echo.New()
+
+	srv := &echoService{}
+	echosvr.RegisterEchoService(e, srv)
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
