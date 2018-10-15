375d89e3c80ad520efe835e03d9653414712c0c0
问天 <wengw@yoozoo.com>
2018-10-15 19:44:14 +0800

    Add ts test for echo

--- b.bat
+++ b.bat
@@ -1,5 +1,6 @@
 .\protoapi.exe gen test\result\go\ test\proto\test.proto --lang=go
 .\protoapi.exe gen test\result\go\ test\proto\echo.proto --lang=go
+.\protoapi.exe gen test\result\ts\ test\proto\echo.proto --lang=ts
 .\protoapi.exe gen test\result\go\ test\proto\calc.proto --lang=go
 .\protoapi.exe gen test\result\go\ test\proto\todolist.proto --lang=go
 REM .\protoapi.exe gen test\result\ts\ test\proto\test.proto --lang=ts
