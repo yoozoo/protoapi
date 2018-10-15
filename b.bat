29bdd260d363b2130b8470a09a64149193799db8
问天 <wengw@yoozoo.com>
2018-10-15 17:18:52 +0800

    Add todolist test

--- b.bat
+++ b.bat
@@ -1,6 +1,7 @@
 .\protoapi.exe gen test\result\go\ test\proto\test.proto --lang=go
 .\protoapi.exe gen test\result\go\ test\proto\echo.proto --lang=go
 .\protoapi.exe gen test\result\go\ test\proto\calc.proto --lang=go
+.\protoapi.exe gen test\result\go\ test\proto\todolist.proto --lang=go
 REM .\protoapi.exe gen test\result\ts\ test\proto\test.proto --lang=ts
 REM .\protoapi.exe gen test\result\ts\fetch test\proto\test.proto --lang=ts-fetch
 REM .\protoapi.exe gen test\result\ts\axios test\proto\test.proto --lang=ts-axios
