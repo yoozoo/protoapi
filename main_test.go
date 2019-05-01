31d672d57cdc984aee856a430ee54abf3684d720
Wuvist <wuvist@gmail.com>
2019-05-01 21:21:40 +0800

    Add current test cases

--- main_test.go
+++ main_test.go
@@ -46,15 +46,47 @@ func TestCmd(t *testing.T) {
 		return
 	}
 
-	test(t, "gen --lang=go test/result/go test/proto/test.proto")
-	// test(t, "gen --lang=go test/result/go test/proto/calc.proto")
-	// test(t, "gen --lang=go test/result/go test/proto/todolist.proto")
-	// test(t, "gen --lang=go test/result/go test/proto/nested.proto")
+	testCmds := `
+	gen --lang=go test/result/go test/proto/test.proto
+	gen --lang=go test/result/go test/proto/echo.proto
+	gen --lang=go test/result/go test/proto/calc.proto
+	gen --lang=go test/result/go test/proto/todolist.proto
+	gen --lang=go test/result/go test/proto/nested.proto
 
-	test(t, "gen --lang=ts test/result/ts test/proto/test.proto")
-	test(t, "gen --lang=spring test/result/ test/proto/test.proto")
+	gen --lang=go test/result/package/go test/proto/package/common.proto
+	gen --lang=go test/result/package/go test/proto/package/gopackage_addReqFull.proto
+	gen --lang=go test/result/package/go test/proto/package/gopackage_addReq.proto
+	gen --lang=go test/result/package/go test/proto/package/gopackage_calcFull.proto
+	gen --lang=go test/result/package/go test/proto/package/gopackage_calc.proto
+	gen --lang=go test/result/package/go test/proto/package/gopackage_calc_warn.proto
+	gen --lang=go test/result/package/go test/proto/package/mixpackage_addReq.proto
+	gen --lang=go test/result/package/go test/proto/package/mixpackage_calc.proto
+	gen --lang=go test/result/package/go test/proto/package/nopackage_calc.proto
+	gen --lang=go test/result/package/go test/proto/package/nopackage_calc_warn.proto
+	gen --lang=go test/result/package/go test/proto/package/package_addReq.proto
+	gen --lang=go test/result/package/go test/proto/package/package_calc_commonerror.proto
+	gen --lang=go test/result/package/go test/proto/package/package_calc.proto
+	gen --lang=go test/result/package/go test/proto/package/package_calc._without_commonerror.proto
 
-	test(t, "gen --lang=phpclient test/result/ test/proto/test.proto")
-	test(t, "gen --lang=yii2 test/result/ test/proto/todolist.proto")
-	test(t, "gen --lang=markdown test/result/ test/proto/login.proto")
+	gen --lang=ts test/result/ts test/proto/test.proto
+	gen --lang=ts-fetch test/result/ts/fetch test/proto/test.proto
+	gen --lang=ts-axios test/result/ts/axios test/proto/test.proto
+
+	gen --lang=spring test/result/ test/proto/test.proto
+
+	gen --lang=phpclient test/result/ test/proto/test.proto
+
+	gen --lang=yii2 test/result/ test/proto/todolist.proto
+
+	gen --lang=markdown test/result/ test/proto/login.proto
+
+	`
+	testCmds = strings.TrimSpace(testCmds)
+
+	for _, cmd := range strings.Split(testCmds, "\n") {
+		cmd = strings.TrimSpace(cmd)
+		if cmd != "" {
+			test(t, cmd)
+		}
+	}
 }
