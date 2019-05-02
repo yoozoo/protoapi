c4bcee0793df1ebf1958349f6e4f400ba18eca8c
Weng Wei <wuvist@gmail.com>
2019-05-02 16:09:22 +0800

    Implement ts-wechat

--- generator/data/tpl/tpl.go
+++ generator/data/tpl/tpl.go
@@ -540,22 +540,23 @@ AAD//6nV5A0iBgAA
 	"/generator/template/ts/service_wechat.gots": {
 		name:    "service_wechat.gots",
 		local:   "generator/template/ts/service_wechat.gots",
-		size:    1398,
+		size:    1559,
 		modtime: 0,
 		compressed: `
-H4sIAAAAAAAC/5yTzU4bSRDH7/0UpRGSZywzxistYu31SvvB7rLSYiSzD9CMy/ag8fRsd9sC9fZhtUs+
-FBI4oBySQxJFkZCQAqccIIGXsTG3vELU02OPwckldevqqn9V/aq7XCySItycvrl+en948W589OL6weHw
-/EnCmWQ0CUkR7M1of290eDJ6+Hh8cnpz9t/46PjHjbXxs/+HF6/HL//9+H5/ePl8dPl2ePVqdHYwPn40
-Oj8YfrgaHx3nCvf2bLD1b+4m2Ax4mEhSLJOwlzAuQREAAKU4jTsIC3I3wRIsbDEWQbUObgflWhr4C5XU
-CAjwf+3HgQxZLDyts+xFmwlalzIPxi2tiYY2Zz0o+GWl/J8jKsQ67aHWja1tUahNe4AOxsipxL94VALk
-nPHfadyKwrgDuUQXowR5oUbIgHLYosKEQx2crpRJtVyufPeNX1le8SuVb/3lperK0sqSUyMEd9Ii7axr
-aKL8yea6fR5VQUgexh0vI5HL9nlUI5qQdLpg0ruhkk8CizqLsPxyNlrbxB7KLmuZLCdhQjpphrkJ24B/
-p4CbyAdhgH/KFviNxJIF57fVzTx4VqaDUxULOQ1IoZn7VHLVnMy6ZhS1nkOhlG/34SaU056oGs9anPSl
-ydXaq8IGZ71Q4PdK+Y2+nN7APxDjAPkPGbYIJczQhPrsSt0MagkcpXKUWjupJ+vB8Wok1eIo+zwGuhMy
-4SuVza61WVcJlArbEOMUieWhtZ1AKYwEaq3AnkFnlEoQsLgddry0hDE/oDLousg51CdjTKxchq55gGgf
-I3AUCYsF3grK+rz1XI1cXkJ7vuxi7HIU8zXCNrjm07C2kfdbVFKo1+tQsAgL3p14Y5LvfsZrzHwJKwF/
-NBvrfkK5QHciPEF717IRsiX7HAWLBuimQlTAnaV7cxoaUojgoveFvuYqbGMgZ/uakyS3T+Sr1LRn/m66
-+/SvfAoAAP//cXrzJ3YFAAA=
+H4sIAAAAAAAC/4yUTY/bRBjH7/MpHlmVbEfpZBeJanEwEi8F9gC7YssJcZg4TxJXjsedGedFZg4Iyoso
+tIcVBzgAQkgrVaI9cWih/TJJ0xtfAc2MY3t7QL1E8fPy9+P//J4Z9HqkB88f/PHsx683j//anf/y7Jt7
+m0c/FIIrzoqU9MBltndub+/d3377/e7+g+cPv9idX7x5erz76cvN4993v37+7993Nk9+3j75c/P0t+3D
+u7uL77aP7m7+ebo7v2gVvrrtil38xrrAs0SkhSK9AUnnBRcKKgIAUFWC5VOEK2pdYB+ujDjPIIohmKI6
+toXvMMWMgAT6bpknKuW5DLWuu6+6TtC6X0cwH2tNNEwEn4NPB1VF386YlB+yOWp9Mrop/WEzA0wxR8EU
+fiyyPqAQXLzP8nGW5lNoJWaYFSj8ISEJz6WCQvB5KtPJGmIIlqsQ4jfs5whUpcghmKOa8XETbhO8MON3
+Ek0qxyWcWlmEIBAoebbAvsCbmKhL9QDL1SfuBZ8GbRCAUurk+52gLJMEpYyMopOBWttFdLd4wtIsggCF
+cKXu7fZZN2U6JJf+mIwmem/NcnXaMacxyrjUVHyEt0qUCuJudeALF/ZDQhZMwIhJcyoQgzdTqogGg8PX
+XqGH147o4eGr9NpBdHRwdOANCcGVPctJDQecoXrL9QalyCKQSqT5NKwdbGVLkQ3N4BaiZI+Iga8FBq7q
+usJh2iKotWt0R2G6vNOTsxue7TCZdAJ4y3J8hmKRJviBGgM9KRzA4L13vVPclekkHMu2wLJp8lbyunky
+W9FRNJO+6EVVUcd9UDDB5jIykeO8KJVp1jqM9tS9XlX0pFRNBj6DHBcoHHkZKuiYCXF3cYLa0z54VdU6
+qbVnI/UEXjgk7Y40FASVEy5N/5gpFoEbtQ/Ok8ivqtoerX0dUjXD3NDbbkU6gcDcAnxi6KZGBeI4Bp+P
+DMF+2GyPEuvOJtWz1A7QzmY4DSbhBVca+CFhKplBgOH/6dn92cuFw30zaX9fokWH1L7MLGL7zYMBzMxN
+he7WMh9e8FxiV/XSfWb32MoZ6i1blrL/AgAA///ENiGuFwYAAA==
 `,
 	},
 
---
+++ test/expected/ts/wechat/AppService.ts
@@ -0,0 +1,302 @@
+/**
+* 该文件生成于protoapi
+* 文件包含前端调用API的代码，供微信小程序使用
+* 文件内代码使用TypeScript
+*/
+import {
+    EnvListRequest,
+    EnvListResponse,
+    KVHistoryRequest,
+    KVHistoryResponse,
+    KeyListRequest,
+    KeyListResponse,
+    KeyValueListRequest,
+    KeyValueListResponse,
+    KeyValueRequest,
+    KeyValueResponse,
+    ProductListRequest,
+    ProductListResponse,
+    RegisterServiceRequest,
+    RegisterServiceResponse,
+    SearchKeyValueListRequest,
+    ServiceListRequest,
+    ServiceListResponse,
+    ServiceSearchRequest,
+    TagListRequest,
+    TagListResponse,
+    UpdateServiceRequest,
+    UpdateServiceResponse,
+    UploadProtoFileRequest,
+    UploadProtoFileResponse,
+    
+} from './AppServiceObjs';
+import { generateUrl, errorHandling } from './helper';
+
+const promisify = (wx) => {
+  return (method) => {
+    return (option) => {
+      return new Promise ((resolve,reject) => {
+        wx[method]({
+          ...option,
+          success:(res) => { resolve(res) },
+          fail: (err) => {reject(err)}
+        })
+      })
+    }
+  }
+}
+
+const wxPromisify = promisify(wx)
+const wxRequest = wxPromisify('request')
+
+var baseUrl = "http://192.168.115.60:8080";
+
+export function SetBaseUrl(url: string) {
+    baseUrl = url;
+}
+
+export function getEnv(params: EnvListRequest): Promise<EnvListResponse | never> {
+  let url: string = generateUrl(baseUrl, "AppService", "getEnv");
+
+  return wxRequest({ url: url, data: params, method:'POST'}).then(res => {
+    if (typeof res.data === 'object') {
+      try {
+        return Promise.resolve(res.data as EnvListResponse)
+      } catch (e) {
+        return Promise.reject(res.data);
+      }
+    }
+    return Promise.reject(res.data);
+  }).catch(err => {
+    // handle error response
+    return errorHandling(err)
+  });
+}
+
+
+export function registerService(params: RegisterServiceRequest): Promise<RegisterServiceResponse | never> {
+  let url: string = generateUrl(baseUrl, "AppService", "registerService");
+
+  return wxRequest({ url: url, data: params, method:'POST'}).then(res => {
+    if (typeof res.data === 'object') {
+      try {
+        return Promise.resolve(res.data as RegisterServiceResponse)
+      } catch (e) {
+        return Promise.reject(res.data);
+      }
+    }
+    return Promise.reject(res.data);
+  }).catch(err => {
+    // handle error response
+    return errorHandling(err)
+  });
+}
+
+
+export function updateService(params: UpdateServiceRequest): Promise<UpdateServiceResponse | never> {
+  let url: string = generateUrl(baseUrl, "AppService", "updateService");
+
+  return wxRequest({ url: url, data: params, method:'POST'}).then(res => {
+    if (typeof res.data === 'object') {
+      try {
+        return Promise.resolve(res.data as UpdateServiceResponse)
+      } catch (e) {
+        return Promise.reject(res.data);
+      }
+    }
+    return Promise.reject(res.data);
+  }).catch(err => {
+    // handle error response
+    return errorHandling(err)
+  });
+}
+
+
+export function uploadProtoFile(params: UploadProtoFileRequest): Promise<UploadProtoFileResponse | never> {
+  let url: string = generateUrl(baseUrl, "AppService", "uploadProtoFile");
+
+  return wxRequest({ url: url, data: params, method:'POST'}).then(res => {
+    if (typeof res.data === 'object') {
+      try {
+        return Promise.resolve(res.data as UploadProtoFileResponse)
+      } catch (e) {
+        return Promise.reject(res.data);
+      }
+    }
+    return Promise.reject(res.data);
+  }).catch(err => {
+    // handle error response
+    return errorHandling(err)
+  });
+}
+
+
+export function getTags(params: TagListRequest): Promise<TagListResponse | never> {
+  let url: string = generateUrl(baseUrl, "AppService", "getTags");
+
+  return wxRequest({ url: url, data: params, method:'POST'}).then(res => {
+    if (typeof res.data === 'object') {
+      try {
+        return Promise.resolve(res.data as TagListResponse)
+      } catch (e) {
+        return Promise.reject(res.data);
+      }
+    }
+    return Promise.reject(res.data);
+  }).catch(err => {
+    // handle error response
+    return errorHandling(err)
+  });
+}
+
+
+export function getProducts(params: ProductListRequest): Promise<ProductListResponse | never> {
+  let url: string = generateUrl(baseUrl, "AppService", "getProducts");
+
+  return wxRequest({ url: url, data: params, method:'POST'}).then(res => {
+    if (typeof res.data === 'object') {
+      try {
+        return Promise.resolve(res.data as ProductListResponse)
+      } catch (e) {
+        return Promise.reject(res.data);
+      }
+    }
+    return Promise.reject(res.data);
+  }).catch(err => {
+    // handle error response
+    return errorHandling(err)
+  });
+}
+
+
+export function getServices(params: ServiceListRequest): Promise<ServiceListResponse | never> {
+  let url: string = generateUrl(baseUrl, "AppService", "getServices");
+
+  return wxRequest({ url: url, data: params, method:'POST'}).then(res => {
+    if (typeof res.data === 'object') {
+      try {
+        return Promise.resolve(res.data as ServiceListResponse)
+      } catch (e) {
+        return Promise.reject(res.data);
+      }
+    }
+    return Promise.reject(res.data);
+  }).catch(err => {
+    // handle error response
+    return errorHandling(err)
+  });
+}
+
+
+export function searchServices(params: ServiceSearchRequest): Promise<ServiceListResponse | never> {
+  let url: string = generateUrl(baseUrl, "AppService", "searchServices");
+
+  return wxRequest({ url: url, data: params, method:'POST'}).then(res => {
+    if (typeof res.data === 'object') {
+      try {
+        return Promise.resolve(res.data as ServiceListResponse)
+      } catch (e) {
+        return Promise.reject(res.data);
+      }
+    }
+    return Promise.reject(res.data);
+  }).catch(err => {
+    // handle error response
+    return errorHandling(err)
+  });
+}
+
+
+export function getKeyList(params: KeyListRequest): Promise<KeyListResponse | never> {
+  let url: string = generateUrl(baseUrl, "AppService", "getKeyList");
+
+  return wxRequest({ url: url, data: params, method:'POST'}).then(res => {
+    if (typeof res.data === 'object') {
+      try {
+        return Promise.resolve(res.data as KeyListResponse)
+      } catch (e) {
+        return Promise.reject(res.data);
+      }
+    }
+    return Promise.reject(res.data);
+  }).catch(err => {
+    // handle error response
+    return errorHandling(err)
+  });
+}
+
+
+export function getKeyValueList(params: KeyValueListRequest): Promise<KeyValueListResponse | never> {
+  let url: string = generateUrl(baseUrl, "AppService", "getKeyValueList");
+
+  return wxRequest({ url: url, data: params, method:'POST'}).then(res => {
+    if (typeof res.data === 'object') {
+      try {
+        return Promise.resolve(res.data as KeyValueListResponse)
+      } catch (e) {
+        return Promise.reject(res.data);
+      }
+    }
+    return Promise.reject(res.data);
+  }).catch(err => {
+    // handle error response
+    return errorHandling(err)
+  });
+}
+
+
+export function searchKeyValueList(params: SearchKeyValueListRequest): Promise<KeyValueListResponse | never> {
+  let url: string = generateUrl(baseUrl, "AppService", "searchKeyValueList");
+
+  return wxRequest({ url: url, data: params, method:'POST'}).then(res => {
+    if (typeof res.data === 'object') {
+      try {
+        return Promise.resolve(res.data as KeyValueListResponse)
+      } catch (e) {
+        return Promise.reject(res.data);
+      }
+    }
+    return Promise.reject(res.data);
+  }).catch(err => {
+    // handle error response
+    return errorHandling(err)
+  });
+}
+
+
+export function updateKeyValue(params: KeyValueRequest): Promise<KeyValueResponse | never> {
+  let url: string = generateUrl(baseUrl, "AppService", "updateKeyValue");
+
+  return wxRequest({ url: url, data: params, method:'POST'}).then(res => {
+    if (typeof res.data === 'object') {
+      try {
+        return Promise.resolve(res.data as KeyValueResponse)
+      } catch (e) {
+        return Promise.reject(res.data);
+      }
+    }
+    return Promise.reject(res.data);
+  }).catch(err => {
+    // handle error response
+    return errorHandling(err)
+  });
+}
+
+
+export function fetchKeyHistory(params: KVHistoryRequest): Promise<KVHistoryResponse | never> {
+  let url: string = generateUrl(baseUrl, "AppService", "fetchKeyHistory");
+
+  return wxRequest({ url: url, data: params, method:'POST'}).then(res => {
+    if (typeof res.data === 'object') {
+      try {
+        return Promise.resolve(res.data as KVHistoryResponse)
+      } catch (e) {
+        return Promise.reject(res.data);
+      }
+    }
+    return Promise.reject(res.data);
+  }).catch(err => {
+    // handle error response
+    return errorHandling(err)
+  });
+}
