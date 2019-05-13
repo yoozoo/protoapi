2941072aa810104c6032d5329aa183ebb65b2b93
zhuql <qzhu001@gmail.com>
2019-05-13 16:20:25 +0800

    update test files

--- test/expected/ts/axios/AppService.ts
+++ test/expected/ts/axios/AppService.ts
@@ -39,7 +39,7 @@ import {
 } from './AppServiceObjs';
 import { generateUrl, errorHandling } from './helper';
 
-var baseUrl = "http://192.168.115.60:8080";
+let baseUrl = "backend";
 
 export function SetBaseUrl(url: string) {
     baseUrl = url;
