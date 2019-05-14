6351418c01f4853c65c67f663d3e32e2aca6dc26
Qinglei <qzhu001@gmail.com>
2019-05-14 13:48:43 +0800

    indent

--- generator/output/ts.go
+++ generator/output/ts.go
@@ -111,9 +111,9 @@ func isCommonError(name string) bool {
 
 func lowerInital(str string) string {
 	for i, v := range str {                                                                                                                                           
-    	return string(unicode.ToLower(v)) + str[i+1:]
+		return string(unicode.ToLower(v)) + str[i+1:]
 	}  
-    return ""
+	return ""
 }
 
 func getCommonErrorName(name string) string {
