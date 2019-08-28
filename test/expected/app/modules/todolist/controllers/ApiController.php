f062ccb567a5edd17a08806f3bc118b3becfdc15
zhao haitao <zhaoht@youzu.com>
2019-08-28 15:35:41 +0800

    fix test case

--- test/expected/app/modules/todolist/controllers/ApiController.php
+++ test/expected/app/modules/todolist/controllers/ApiController.php
@@ -5,6 +5,7 @@ namespace app\modules\todolist\controllers;
 use app\modules\todolist\models;
 use Yii;
 use yii\web\Controller;
+use yii\base\InlineAction;
 use Yoozoo\ProtoApi;
 
 class ApiController extends Controller
@@ -27,10 +28,9 @@ class ApiController extends Controller
                 'class' => \app\modules\todolist\AuthHandler::className(),
             ];
         }
+        // cors need to be the first filter so it will be effective in all request
         if (class_exists("\\app\\modules\\todolist\\CorsHandler")){
-            $behaviors['authenticator'] = [
-                'class' => \app\modules\todolist\CorsHandler::className(),
-            ];
+            return ["cors" => [ 'class' => \app\modules\todolist\CorsHandler::className() ]] + $behaviors;
         }
 
         return $behaviors;
@@ -64,4 +64,13 @@ class ApiController extends Controller
         throw new ProtoApi\GeneralException("return type of 'list' incorrect.");
     }
     
+    private $methods = [ 'add' => 'actionAdd',  'list' => 'actionList', ];
+
+    public function createAction($id)
+    {
+        if (isset($this->methods[$id])) {
+            return new InlineAction($id, $this, $this->methods[$id]);
+        }
+        return null;
+    }
 }
